5ff4f8fee71d3c14733b3a9f3a4200aa8e29b0bc
zeitun-adan <zeitun.adan@student.moringaschool.com>
2022-06-30 14:52:31 +0300

    commit tree 68a4ef66f78858b8096d610b2f3ec47582e9aa31

--- index.js
+++ index.js
@@ -1,1 +1,76 @@
 // Your code here
+function createEmployeeRecord(array){
+    let employeeRecord = {
+        firstName : array[0],
+        familyName : array[1],
+        title : array[2],
+        payPerHour : array[3],
+        timeInEvents : [],
+        timeOutEvents : [] 
+    }
+    return employeeRecord;
+}
+
+function createEmployeeRecords(records){
+    let employeeRecords = records.map(record => createEmployeeRecord(record))
+    return employeeRecords
+}
+
+function createTimeInEvent(empRec, dateStamp){
+    let [date, hour] = dateStamp.split(' ')
+
+    empRec.timeInEvents.push({
+        type : "TimeIn",
+        hour: parseInt(hour,10),
+        date,
+    })
+
+    return empRec
+}
+
+function createTimeOutEvent(empRec, dateStamp){
+    let [date, hour] = dateStamp.split(' ')
+
+    empRec.timeOutEvents.push({
+        type: "TimeOut",
+        hour : parseInt(hour, 10),
+        date
+    })
+
+    return empRec
+}
+
+function hoursWorkedOnDate(employee, workDate){
+    let inTheEvent = employee.timeInEvents.find((e)=>{
+        return e.date === workDate
+    })
+
+    let outOfTheEvent = employee.timeOutEvents.find((e)=>{
+        return e.date === workDate
+    })
+
+    return (outOfTheEvent.hour - inTheEvent.hour) / 100
+}
+
+function wagesEarnedOnDate(employee, dateWork){
+    let wageEarned = hoursWorkedOnDate(employee, dateWork) * employee.payPerHour
+    return parseFloat(wageEarned.toString())
+}
+
+function allWagesFor(employee){
+    let eligbleDates = employee.timeInEvents.map((e)=>{
+        return e.date
+    })
+
+    let payable = eligbleDates.reduce((memo, d)=>{
+        return memo + wagesEarnedOnDate(employee,d)
+    }, 0)
+
+    return payable
+}
+
+function calculatePayroll(arrayOfEmployeeRecords){
+    return arrayOfEmployeeRecords.reduce((memo, rec)=>{
+        return memo + allWagesFor(rec)
+    }, 0)
+}
